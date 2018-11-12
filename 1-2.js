eb961237c94312d1b97833dc70af2b65655ae14b
libraprince <haimingwei104229@gmail.com>
2018-11-13 00:03:20 +0800

    one file

---
+++ 1-2.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const  port =8080;
+const server = http.createServer((req,res)=>{
+
+    res.end('nihao');
+});
+server.listen(port,()=>{
+    console.log('server listening on:http://localhost:%s',port);
+});
